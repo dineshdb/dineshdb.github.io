2b50fc0c974db2c0917bc44bfbffffd62b9512ec
dineshdb <dineshdb@dbhattarai.info.np>
2023-08-25 17:26:38 +0545

    Snapshot

---
+++ astro.config.mjs
@@ -0,0 +1,43 @@
+import { defineConfig, sharpImageService } from "astro/config";
+import mdx from "@astrojs/mdx";
+import UnoCSS from "unocss/astro";
+import compressor from "astro-compressor";
+import preact from "@astrojs/preact";
+
+import unocss from "@unocss/astro";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://dbhattarai.info.np",
+  integrations: [
+    mdx(),
+    compressor(),
+    UnoCSS({
+      // injectReset: true,
+    }),
+    preact({
+      compat: true,
+    }),
+    unocss(),
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: "nord",
+    },
+    remarkPlugins: ["remark-gfm", "remark-smartypants"],
+    rehypePlugins: [
+      [
+        "rehype-external-links",
+        {
+          target: "_blank",
+        },
+      ],
+    ],
+  },
+  experimental: {
+    assets: true,
+  },
+  image: {
+    service: sharpImageService(),
+  },
+});
